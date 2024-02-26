0024ce46a0f3d1b77ba9d031d7c7cb77db40307c
gjisoo <gsoo.se00@gmail.com>
2024-02-26 15:44:19 +0900

    first-commit

---
+++ cmb/src/main/java/gjisoo_github_io/cmb/CmbApplication.java
@@ -0,0 +1,13 @@
+package gjisoo_github_io.cmb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmbApplication.class, args);
+	}
+
+}
---
+++ cmb/src/test/java/gjisoo_github_io/cmb/CmbApplicationTests.java
@@ -0,0 +1,13 @@
+package gjisoo_github_io.cmb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
